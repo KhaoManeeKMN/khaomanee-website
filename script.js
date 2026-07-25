c8455b2b223c744dd0a591f4f7135227b91b1165
KhaoManeeKMN <ibernautic2016@icloud.com>
2026-07-25 12:39:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function copyContract() {
+
   const contract = "B2SDZA6De8Tr9c6dJvVGVSPyEVrRp5k7kVeSHnJdpump";
+
   navigator.clipboard.writeText(contract);
-  alert("Contract copied!");
+
+  const button = document.querySelector(".contract button");
+
+  const originalText = button.innerText;
+
+  button.innerText = "✅ Copied!";
+
+  button.style.background = "#14F195";
+  button.style.color = "#000";
+
+  setTimeout(() => {
+    button.innerText = originalText;
+  }, 2000);
+
 }
